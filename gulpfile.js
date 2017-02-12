a2a432699664b42774f72e1644bf9bdcef4c9fa6
James Florentino <jamesflorentino@gmail.com>
2017-02-13 02:19:56 +0800

    Add kss documentation tool

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 const gulp = require('gulp')
 const nib = require('nib')
-const stylus = require('gulp-stylus')
 const shell = require('gulp-shell')
+const stylus = require('gulp-stylus')
+const connect = require('gulp-connect')
 
 const paths = {
   stylusAll: ['./src/**/*.styl'],
   stylusEntry: ['./src/app.styl'],
 }
 
+gulp.task('connect', function() {
+  connect.server({ port: 4000 })
+})
 gulp.task('stylus', function() {
   return gulp.src(paths.stylusEntry)
     .pipe(stylus({ compress: true, use: nib() }))
@@ -16,8 +20,11 @@ gulp.task('stylus', function() {
 
 gulp.task('lint', shell.task(['stylint src']))
 
+const cdnurl = 'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.css'
+gulp.task('docs', shell.task([`kss --source src --destination docs --css "/dist/app.css" --css "${cdnurl}"`]))
+
 gulp.task('watch', function() {
-  gulp.watch(paths.stylusAll, ['lint', 'stylus'])
+  gulp.watch(paths.stylusAll, ['lint', 'stylus', 'docs'])
 })
 
-gulp.task('default', ['lint', 'stylus', 'watch'])
+gulp.task('default', ['connect', 'lint', 'stylus', 'docs', 'watch'])
