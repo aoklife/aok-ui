6840635153147ee776f7fc0fae8a019870b9bdcf
James Florentino <jamesflorentino@gmail.com>
2017-02-13 12:54:42 +0800

    Add `build` task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,14 @@ const paths = {
 gulp.task('connect', function() {
   connect.server({ port: 4000 })
 })
+
 gulp.task('stylus', function() {
+  return gulp.src(paths.stylusEntry)
+    .pipe(stylus({ use: nib() }))
+    .pipe(gulp.dest('./tmp'))
+})
+
+gulp.task('build', function() {
   return gulp.src(paths.stylusEntry)
     .pipe(stylus({ compress: true, use: nib() }))
     .pipe(gulp.dest('./dist'))
@@ -21,7 +28,7 @@ gulp.task('stylus', function() {
 gulp.task('lint', shell.task(['stylint src']))
 
 const cdnurl = 'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.css'
-gulp.task('docs', shell.task([`kss --source src --destination docs --css "/dist/app.css" --css "${cdnurl}"`]))
+gulp.task('docs', shell.task([`kss --source src --destination docs --css "/tmp/app.css" --css "${cdnurl}"`]))
 
 gulp.task('watch', function() {
   gulp.watch(paths.stylusAll, ['lint', 'stylus', 'docs'])
